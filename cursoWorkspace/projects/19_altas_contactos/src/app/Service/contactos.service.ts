029d9e136f586493d272cc11623e1c3dff7026d4
AinhoaAlonso <ainhoaalonso88@gmail.com>
2025-05-27 10:53:33 +0200

    Ejercicio 19 altas

--- cursoWorkspace/projects/19_altas_contactos/src/app/Service/contactos.service.ts
+++ cursoWorkspace/projects/19_altas_contactos/src/app/Service/contactos.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Contacto } from '../../model/Contacto';
-import { catchError, map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +11,16 @@ export class ContactosService {
 
   constructor( private http:HttpClient) { }
 
-  alta(contacto:Contacto):Observable<boolean>{
-    return this.http.post(`${this.url}/alta`,contacto)
+  //El error 409 se captura en el componente
+  /*alta(contacto:Contacto):Observable<any>{
+    return this.http.post(this.url+"/alta",contacto);
+  }*/
+  //el posible error 409 se captura aquí en el service
 
+  alta(contacto:Contacto):Observable<boolean>{
+    return this.http.post(this.url+"/alta",contacto)
+    .pipe(map(()=>true),
+              catchError(e=>of(false)));
   }
+
 }
--- cursoWorkspace/projects/19_altas_contactos/src/app/components/alta/alta.component.ts
+++ cursoWorkspace/projects/19_altas_contactos/src/app/components/alta/alta.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ContactosService } from '../../Service/contactos.service';
 import { Contacto } from '../../../model/Contacto';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-alta',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './alta.component.html',
   styleUrl: './alta.component.css'
 })
@@ -14,9 +16,19 @@ export class AltaComponent {
   constructor(private contactosService: ContactosService){}
 
   alta(){
-    this.contactosService.alta(this.contacto).subscribe({
+    //Si controlamos el error aqui en el componente
+    /*this.contactosService.alta(this.contacto).subscribe({
       next: data => this.mensaje = "Contacto agregado",
       error: err => this.mensaje = " No se pudo dar de alta el contacto!"
+    })*/
+
+    this.contactosService.alta(this.contacto)
+    .subscribe(data =>{
+      if(data){
+        this.mensaje = "Contacto agregado";
+      }else{
+        this.mensaje = "No se pudo dar de alta el contacto!";
+      }
     })
   }
 
